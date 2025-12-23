ed2f369f5d3bba5fc767c93f8c05cb3c6fd3d0cd
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-12-22 23:49:50 -0500

    feat: Add TypeORM database module for asynchronous configuration and update production DB password.

--- src/config/database/database.module.ts
+++ src/config/database/database.module.ts
@@ -6,30 +6,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     imports: [
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => {
-                const socketPath = configService.get<string>('DB_SOCKET_PATH');
-                const config: any = {
-                    type: 'mysql',
-                    username: configService.get<string>('DB_USERNAME'),
-                    password: configService.get<string>('DB_PASSWORD'),
-                    database: configService.get<string>('DB_DATABASE'),
-                    entities: [__dirname + '/../../**/*.typeorm-entity{.ts,.js}'],
-                    synchronize: configService.get<boolean>('DB_SYNCHRONIZE', false),
-                    logging: configService.get<boolean>('DB_LOGGING', false),
-                    charset: 'utf8mb4',
-                    timezone: 'Z',
-                };
-
-                // Use socket path if provided, otherwise use host/port
-                if (socketPath) {
-                    config.socketPath = socketPath;
-                } else {
-                    config.host = configService.get<string>('DB_HOST');
-                    config.port = configService.get<number>('DB_PORT');
-                }
-
-                return config;
-            },
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
+                entities: [__dirname + '/../../**/*.typeorm-entity{.ts,.js}'],
+                synchronize: configService.get<boolean>('DB_SYNCHRONIZE', false),
+                logging: configService.get<boolean>('DB_LOGGING', false),
+                charset: 'utf8mb4',
+                timezone: 'Z',
+            }),
             inject: [ConfigService],
         }),
     ],
